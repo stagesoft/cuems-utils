b0df2314253d824a724485c324449186cccfa15e
adria <atom@despatx>
2025-09-05 12:06:03 +0200

    release: 0.0.9rc7 | fix: improve properties typing

--- src/cuemsutils/cues/MediaCue.py
+++ src/cuemsutils/cues/MediaCue.py
@@ -14,167 +14,6 @@ REGION_REQ_ITEMS = {
     'out_time': None
 }
 
-class MediaCue(Cue):
-    """Base class for media-related cues (audio and video).
-    
-    This class extends Cue to provide common functionality for media playback,
-    including media file handling and output routing.
-    """
-    
-    def __init__(self, init_dict = None):
-        """Initialize a MediaCue.
-        
-        Args:
-            init_dict (dict, optional): Dictionary containing initialization values.
-                If not provided, default values from REQ_ITEMS will be used.
-        """
-        if not init_dict:
-            init_dict = REQ_ITEMS
-        else:
-            init_dict = ensure_items(init_dict, REQ_ITEMS)
-        super().__init__(init_dict)
-
-    def get_Media(self):
-        """Get the media object associated with this cue.
-        
-        Returns:
-            Media: The media object containing file and region information.
-        """
-        return super().__getitem__('Media')
-
-    def set_Media(self, value):
-        """Set the media object for this cue.
-        
-        Args:
-            value (Media or dict): The media object or dictionary to create one.
-        """
-        if not isinstance(value, Media):
-            value = Media(value)
-        super().__setitem__('Media', value)
-
-    media = property(get_Media, set_Media)
-
-    def get_outputs(self):
-        """Get the output routing configuration.
-        
-        Returns:
-            list: The list of output configurations.
-        """
-        return super().__getitem__('outputs')
-
-    def set_outputs(self, outputs):
-        """Set the output routing configuration.
-        
-        Args:
-            outputs (list): The list of output configurations.
-        """
-        super().__setitem__('outputs', outputs)
-
-    outputs = property(get_outputs, set_outputs)
-
-    def items(self):
-        """Get all items in the cue as a dictionary.
-        
-        Returns:
-            dict_items: A view of the cue's items, with required items included.
-        """
-        x = dict(super().items())
-        for k in REQ_ITEMS.keys():
-            x[k] = self[k]
-        return x.items()
-
-class Media(CuemsDict):
-    """A class representing a media file with associated regions."""
-    
-    def __init__(self, init_dict = None):
-        """Initialize a Media object.
-        
-        Args:
-            init_dict (dict, optional): Dictionary containing initialization values.
-                If provided, will be used to set initial properties.
-        """
-        if init_dict:
-            self.setter(init_dict)
-    
-    def get_file_name(self):
-        """Get the media file name.
-        
-        Returns:
-            str: The name of the media file.
-        """
-        return super().__getitem__('file_name')
-
-    def set_file_name(self, file_name):
-        """Set the media file name.
-        
-        Args:
-            file_name (str): The new media file name.
-        """
-        super().__setitem__('file_name', file_name)
-
-    file_name = property(get_file_name, set_file_name)
-
-    def get_id(self):
-        """Get the UUID of the media file.
-        
-        Returns:
-            str: The UUID of the media file.
-        """
-        return super().__getitem__('id')
-
-    def set_id(self, id):
-        """Set the UUID of the media file.
-        
-        Args:
-            id (str): The new UUID of the media file.
-        """
-        id = Uuid(id)
-        super().__setitem__('id', id)
-
-    id = property(get_id, set_id)
-
-    def get_duration(self):
-        """Get the duration of the media file.
-        
-        Returns:
-            str: The duration of the media file.
-        """
-        return super().__getitem__('duration')
-    
-    def set_duration(self, duration):
-        """Set the duration of the media file.
-        
-        Args:
-            duration (str): The new duration of the media file.
-        """
-        super().__setitem__('duration', duration)
-
-    duration = property(get_duration, set_duration)
-
-    def get_regions(self):
-        """Get the list of regions in the media file.
-        
-        Returns:
-            list: The list of Region objects.
-        """
-        return super().__getitem__('regions')
-
-    def set_regions(self, regions):
-        """Set the list of regions in the media file.
-        
-        Args:
-            regions (list or Region): A list of regions or a single region.
-                If not already Region objects, they will be converted.
-        """
-        if not isinstance(regions, list):
-            regions = [regions]
-        for r in regions:
-            if not isinstance(r, Region):
-                r = Region(r)
-        super().__setitem__('regions', regions)
-
-    regions = property(get_regions, set_regions)
-
 class Region(CuemsDict):
     """A class representing a region within a media file."""
     
@@ -272,3 +111,164 @@ class Region(CuemsDict):
             dict: A dictionary representation of the region.
         """
         return {type(self).__name__: dict(self.items())}
+
+class Media(CuemsDict):
+    """A class representing a media file with associated regions."""
+    
+    def __init__(self, init_dict = None):
+        """Initialize a Media object.
+        
+        Args:
+            init_dict (dict, optional): Dictionary containing initialization values.
+                If provided, will be used to set initial properties.
+        """
+        if init_dict:
+            self.setter(init_dict)
+    
+    def get_file_name(self):
+        """Get the media file name.
+        
+        Returns:
+            str: The name of the media file.
+        """
+        return super().__getitem__('file_name')
+
+    def set_file_name(self, file_name):
+        """Set the media file name.
+        
+        Args:
+            file_name (str): The new media file name.
+        """
+        super().__setitem__('file_name', file_name)
+
+    file_name = property(get_file_name, set_file_name)
+
+    def get_id(self):
+        """Get the UUID of the media file.
+        
+        Returns:
+            str: The UUID of the media file.
+        """
+        return super().__getitem__('id')
+
+    def set_id(self, id):
+        """Set the UUID of the media file.
+        
+        Args:
+            id (str): The new UUID of the media file.
+        """
+        id = Uuid(id)
+        super().__setitem__('id', id)
+
+    id = property(get_id, set_id)
+
+    def get_duration(self):
+        """Get the duration of the media file.
+        
+        Returns:
+            str: The duration of the media file.
+        """
+        return super().__getitem__('duration')
+    
+    def set_duration(self, duration):
+        """Set the duration of the media file.
+        
+        Args:
+            duration (str): The new duration of the media file.
+        """
+        super().__setitem__('duration', duration)
+
+    duration = property(get_duration, set_duration)
+
+    def get_regions(self):
+        """Get the list of regions in the media file.
+        
+        Returns:
+            list: The list of Region objects.
+        """
+        return super().__getitem__('regions')
+
+    def set_regions(self, regions):
+        """Set the list of regions in the media file.
+        
+        Args:
+            regions (list or Region): A list of regions or a single region.
+                If not already Region objects, they will be converted.
+        """
+        if not isinstance(regions, list):
+            regions = [regions]
+        for r in regions:
+            if not isinstance(r, Region):
+                r = Region(r)
+        super().__setitem__('regions', regions)
+
+    regions: list[Region] = property(get_regions, set_regions)
+
+class MediaCue(Cue):
+    """Base class for media-related cues (audio and video).
+    
+    This class extends Cue to provide common functionality for media playback,
+    including media file handling and output routing.
+    """
+    
+    def __init__(self, init_dict = None):
+        """Initialize a MediaCue.
+        
+        Args:
+            init_dict (dict, optional): Dictionary containing initialization values.
+                If not provided, default values from REQ_ITEMS will be used.
+        """
+        if not init_dict:
+            init_dict = REQ_ITEMS
+        else:
+            init_dict = ensure_items(init_dict, REQ_ITEMS)
+        super().__init__(init_dict)
+
+    def get_Media(self):
+        """Get the media object associated with this cue.
+        
+        Returns:
+            Media: The media object containing file and region information.
+        """
+        return super().__getitem__('Media')
+
+    def set_Media(self, value):
+        """Set the media object for this cue.
+        
+        Args:
+            value (Media or dict): The media object or dictionary to create one.
+        """
+        if not isinstance(value, Media):
+            value = Media(value)
+        super().__setitem__('Media', value)
+
+    media: Media = property(get_Media, set_Media)
+
+    def get_outputs(self):
+        """Get the output routing configuration.
+        
+        Returns:
+            list: The list of output configurations.
+        """
+        return super().__getitem__('outputs')
+
+    def set_outputs(self, outputs):
+        """Set the output routing configuration.
+        
+        Args:
+            outputs (list): The list of output configurations.
+        """
+        super().__setitem__('outputs', outputs)
+
+    outputs = property(get_outputs, set_outputs)
+
+    def items(self):
+        """Get all items in the cue as a dictionary.
+        
+        Returns:
+            dict_items: A view of the cue's items, with required items included.
+        """
+        x = dict(super().items())
+        for k in REQ_ITEMS.keys():
+            x[k] = self[k]
+        return x.items()
